63c38e357d8dc02b630424d9cf124cc2f1d46850
lupayao <96031824+lupayao@users.noreply.github.com>
2025-06-04 11:34:02 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Sad)
-    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wedding), music.PlaybackMode.InBackground)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Blues), music.PlaybackMode.InBackground)
 })
-basic.showIcon(IconNames.Happy)
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+    basic.pause(5000)
+})
